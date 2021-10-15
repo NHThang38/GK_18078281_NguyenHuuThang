d3d39dfc4aaac5b6e77ce7b196e37bbda551ed48
NguyenHuuThang <thangtb100@gmail.com>
2021-10-15 16:53:54 +0700

    cau 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView play, prev, next, imageView, imgXoay;
+    TextView songTitle, songSinger;
+    SeekBar mSeekBarTime, mSeekBarVol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imgXoay = findViewById(R.id.imageView7);
+
+        Animation xoay = AnimationUtils.loadAnimation(this, R.anim.xoay);
+        imgXoay.startAnimation(xoay);
     }
 }
